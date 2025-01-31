8c2a407682f4548374150588b212b7692e7a31a8
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-01-31 12:33:06 +0100

    build: initialize spring boot project

---
+++ src/test/java/com/archop/archop/ArchopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archop.archop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
